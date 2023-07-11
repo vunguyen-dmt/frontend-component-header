9dd6a4e6da4720026621cf4388aa0fe0c3a1a885
Vu Nguyen <vu.nguyen@dmttek.com>
2023-07-11 20:23:36 +0700

    dist

--- src/index.jsx
+++
@@ -1,8 +0,0 @@
-import Header from './Header';
-import LearningHeader from './learning-header/LearningHeader';
-import messages from './i18n/index';
-import StudioHeader from './StudioHeader';
-
-export { LearningHeader, messages, StudioHeader };
-
-export default Header;
