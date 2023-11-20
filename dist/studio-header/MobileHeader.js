cbceab2e67dbd6ba606c478c01f1bb1bfb4074ea
Vu Nguyen <vu.nguyen@dmttek.com>
2023-11-20 08:58:56 +0000

    dist

---
+++ dist/studio-header/MobileHeader.js
@@ -0,0 +1,79 @@
+var _excluded = ["mainMenuDropdowns"];
+function _extends() { _extends = Object.assign ? Object.assign.bind() : function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; }; return _extends.apply(this, arguments); }
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i]; return arr2; }
+function _iterableToArrayLimit(arr, i) { var _i = null == arr ? null : "undefined" != typeof Symbol && arr[Symbol.iterator] || arr["@@iterator"]; if (null != _i) { var _s, _e, _x, _r, _arr = [], _n = !0, _d = !1; try { if (_x = (_i = _i.call(arr)).next, 0 === i) { if (Object(_i) !== _i) return; _n = !1; } else for (; !(_n = (_s = _x.call(_i)).done) && (_arr.push(_s.value), _arr.length !== i); _n = !0); } catch (err) { _d = !0, _e = err; } finally { try { if (!_n && null != _i["return"] && (_r = _i["return"](), Object(_r) !== _r)) return; } finally { if (_d) throw _e; } } return _arr; } }
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
+function _objectWithoutProperties(source, excluded) { if (source == null) return {}; var target = _objectWithoutPropertiesLoose(source, excluded); var key, i; if (Object.getOwnPropertySymbols) { var sourceSymbolKeys = Object.getOwnPropertySymbols(source); for (i = 0; i < sourceSymbolKeys.length; i++) { key = sourceSymbolKeys[i]; if (excluded.indexOf(key) >= 0) continue; if (!Object.prototype.propertyIsEnumerable.call(source, key)) continue; target[key] = source[key]; } } return target; }
+function _objectWithoutPropertiesLoose(source, excluded) { if (source == null) return {}; var target = {}; var sourceKeys = Object.keys(source); var key, i; for (i = 0; i < sourceKeys.length; i++) { key = sourceKeys[i]; if (excluded.indexOf(key) >= 0) continue; target[key] = source[key]; } return target; }
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { useToggle, ModalPopup } from '@edx/paragon';
+import HeaderBody from './HeaderBody';
+import MobileMenu from './MobileMenu';
+var MobileHeader = function MobileHeader(_ref) {
+  var mainMenuDropdowns = _ref.mainMenuDropdowns,
+    props = _objectWithoutProperties(_ref, _excluded);
+  var _useToggle = useToggle(false),
+    _useToggle2 = _slicedToArray(_useToggle, 4),
+    isOpen = _useToggle2[0],
+    close = _useToggle2[2],
+    toggle = _useToggle2[3];
+  var _useState = useState(null),
+    _useState2 = _slicedToArray(_useState, 2),
+    target = _useState2[0],
+    setTarget = _useState2[1];
+  return /*#__PURE__*/React.createElement(React.Fragment, null, /*#__PURE__*/React.createElement(HeaderBody, _extends({}, props, {
+    isMobile: true,
+    setModalPopupTarget: setTarget,
+    toggleModalPopup: toggle,
+    isModalPopupOpen: isOpen
+  })), /*#__PURE__*/React.createElement(ModalPopup, {
+    hasArrow: true,
+    placement: "bottom",
+    positionRef: target,
+    isOpen: isOpen,
+    onClose: close,
+    onEscapeKey: close,
+    className: "mobile-menu-container"
+  }, /*#__PURE__*/React.createElement(MobileMenu, {
+    mainMenuDropdowns: mainMenuDropdowns
+  })));
+};
+MobileHeader.propTypes = {
+  studioBaseUrl: PropTypes.string.isRequired,
+  logoutUrl: PropTypes.string.isRequired,
+  number: PropTypes.string,
+  org: PropTypes.string,
+  title: PropTypes.string,
+  logo: PropTypes.string,
+  logoAltText: PropTypes.string,
+  authenticatedUserAvatar: PropTypes.string,
+  username: PropTypes.string,
+  isAdmin: PropTypes.bool,
+  mainMenuDropdowns: PropTypes.arrayOf(PropTypes.shape({
+    id: PropTypes.string,
+    buttonTitle: PropTypes.string,
+    items: PropTypes.arrayOf(PropTypes.shape({
+      href: PropTypes.string,
+      title: PropTypes.string
+    }))
+  })),
+  outlineLink: PropTypes.string
+};
+MobileHeader.defaultProps = {
+  logo: null,
+  logoAltText: null,
+  number: null,
+  org: null,
+  title: null,
+  authenticatedUserAvatar: null,
+  username: null,
+  isAdmin: false,
+  mainMenuDropdowns: [],
+  outlineLink: null
+};
+export default MobileHeader;
+//# sourceMappingURL=MobileHeader.js.map
